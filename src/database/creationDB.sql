f98f82a25d5c7b6b1faac7690f527c33739fbfdd
Mohamad <douri_m@etna-alternance.net>
2025-01-30 15:44:46 +0100

    creationDB.sql: a script that allows the creation of the Maquettes_avion database, without inserting data into the database

---
+++ src/database/creationDB.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE IF NOT EXISTS Maquettes_avion;
+
+USE DATABASE Maquettes_avion;
+
+CREATE TABLE `Produit` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `prix_unitaire` FLOAT NOT NULL,
+  `quantite_stock` INT NOT NULL,
+  `categorie_id` INT NOT NULL,
+  `fournisseur_id` INT NOT NULL
+);
+
+CREATE TABLE `Categories` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE `Fournisseurs` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `prenom` VARCHAR(45) NOT NULL,
+  `adresse` VARCHAR(100) NOT NULL,
+  `tel` INT NOT NULL
+);
+
+CREATE TABLE `Client` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `prenom` VARCHAR(45) NOT NULL,
+  `adresse` VARCHAR(100) NOT NULL,
+  `tel` INT NOT NULL
+);
+
+CREATE TABLE `Commandes` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `client_id` INT NOT NULL,
+  `date_commande` DATETIME NOT NULL,
+  `statut` ENUM(En cours,Livrée) NOT NULL
+);
+
+CREATE TABLE `Lignes_Commande` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `commande_id` INT NOT NULL,
+  `produit_id` INT NOT NULL,
+  `quantite` INT NOT NULL,
+  `prix_unitaire_applique` FLOAT NOT NULL
+);
+
+ALTER TABLE `Produit` ADD FOREIGN KEY (`categorie_id`) REFERENCES `Categories` (`id`);
+
+ALTER TABLE `Produit` ADD FOREIGN KEY (`fournisseur_id`) REFERENCES `Fournisseurs` (`id`);
+
+ALTER TABLE `Commandes` ADD FOREIGN KEY (`client_id`) REFERENCES `Client` (`id`);
+
+ALTER TABLE `Lignes_Commande` ADD FOREIGN KEY (`commande_id`) REFERENCES `Commandes` (`id`);
+
+ALTER TABLE `Lignes_Commande` ADD FOREIGN KEY (`produit_id`) REFERENCES `Produit` (`id`);
